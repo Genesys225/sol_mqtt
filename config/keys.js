58cacbcb6c179a2231a57d58c50e46f2716adf5c
Genesys <you@example.com>
2018-11-24 20:49:52 +0200

    mongo env var

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,9 @@
+var isWin = process.platform;
+var mongoURI =
+  isWin === "win32"
+    ? "mongodb://localhost:27017/sol"
+    : "mongodb://10.8.0.13:27017/sol";
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/sol",
+  mongoURI,
   secretOrKey: "secret"
 };
