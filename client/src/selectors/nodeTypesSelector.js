3c60a7f94cd9eaeecbe5796aa5a98b880ba6176e
Genesys <you@example.com>
2018-11-26 15:07:50 +0200

    add assetd to gitignorr

--- client/src/selectors/nodeTypesSelector.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "reselect";
-// Select function to collect state parts
-const changeDetectorSelector = state => state.telemetry;
-
-const getTypes = nodes => {
-  return nodes.nodes;
-};
-
-export default createSelector(changeDetectorSelector, getTypes);
