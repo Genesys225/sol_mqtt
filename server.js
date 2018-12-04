82e6512bfe5f33a021f2c534d33935c1ef2aaa01
Genesys <you@example.com>
2018-12-04 11:49:31 +0200

    if env server static

--- server.js
+++ server.js
@@ -41,24 +41,15 @@ server.use("/api/telemetry", telemetry);
 
 // enable "client" access to it's local depedncies (bootstrap, fonts, etc)
 if (process.env.NODE_ENV === "production") {
-  server.use(express.static("client/build"));
-  server.use(
-    express.static(__dirname + "/node_modules/jquery/dist/jquery.slim.min.js")
-  );
-  server.use(
-    express.static(
-      __dirname + "/node_modules/bootstrap/dist/js/bootstrap.min.js"
-    )
-  );
-  server.use(
-    express.static(
-      __dirname + "node_modules/@fortawesome/fontawesome-free/js/all.js"
-    )
-  );
+  server.use(express.static(path.join(__dirname + "client/build")));
+  //server.use(express.static(__dirname + "/js/jquery.slim.min.js"));
+  //server.use(express.static(__dirname + "/js/bootstrap.min.js"));
+  //server.use(express.static(__dirname + "/js/all.js"));
   server.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
   });
 } else {
+  server.use(express.static("client/build"));
   server.use(express.static(__dirname + "/client"));
   server.use(express.static(__dirname + "/client/public"));
 }
