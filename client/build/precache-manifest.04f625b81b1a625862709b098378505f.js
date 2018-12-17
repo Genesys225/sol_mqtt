ef311f7878eaf16fb6cabbb6eb7288b43dde27a1
Genesys <you@example.com>
2018-12-17 11:41:45 +0200

    react app dotenv

---
+++ client/build/precache-manifest.04f625b81b1a625862709b098378505f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "14df115190d2101edfc80469ac264938",
+    "url": "/static/media/ajax_loader_blue_256.14df1151.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85d1f96133ef1f64e17f",
+    "url": "/static/js/main.85d1f961.chunk.js"
+  },
+  {
+    "revision": "aeabf64d3fbf9608766a",
+    "url": "/static/js/1.aeabf64d.chunk.js"
+  },
+  {
+    "revision": "85d1f96133ef1f64e17f",
+    "url": "/static/css/main.b6939990.chunk.css"
+  },
+  {
+    "revision": "32882ff6b559cee6d679de6052ba9553",
+    "url": "/index.html"
+  }
+];
